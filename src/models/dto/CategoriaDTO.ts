3db0708bd8a3defe452e46e9b8b4dd6046b00522
julio Rivas <juliorivasdev@gmail.com>
2025-06-08 19:12:57 -0300

    Rename dto_tmp → dto (lowercase)

--- src/models/dto/CategoriaDTO.ts
+++ src/models/dto/CategoriaDTO.ts
@@ -1,51 +1,6 @@
-import { ImagenDTO } from "./ImagenDTO";
-
 export class CategoriaDTO {
   private idCategoria: number;
   private nombre: string;
-  private idCategoriaPadre: number;
-  private imagenDto: ImagenDTO;
-
-  constructor(idCategoria: number, nombre: string, idCategoriaPadre: number, imagenDto: ImagenDTO) {
-    this.idCategoria = idCategoria;
-    this.nombre = nombre;
-    this.idCategoriaPadre = idCategoriaPadre;
-    this.imagenDto = imagenDto;
-  }
-
-  // Getter y Setter para idCategoria
-  public getIdCategoria(): number {
-    return this.idCategoria;
-  }
-
-  public setIdCategoria(idCategoria: number): void {
-    this.idCategoria = idCategoria;
-  }
-
-  // Getter y Setter para nombre
-  public getNombre(): string {
-    return this.nombre;
-  }
-
-  public setNombre(nombre: string): void {
-    this.nombre = nombre;
-  }
-
-  // Getter y Setter para idCategoriaPadre
-  public getIdCategoriaPadre(): number {
-    return this.idCategoriaPadre;
-  }
-
-  public setIdCategoriaPadre(idCategoriaPadre: number): void {
-    this.idCategoriaPadre = idCategoriaPadre;
-  }
-
-  // Getter y Setter para imagenDto
-  public getImagenDto(): ImagenDTO {
-    return this.imagenDto;
-  }
-
-  public setImagenDto(imagenDto: ImagenDTO): void {
-    this.imagenDto = imagenDto;
-  }
+  private categoriaPadre: CategoriaDTO | null;
+  private imagen: string | null;
 }
--- src/models/dto_tmp/CategoriaDTO.ts
+++
@@ -1,6 +0,0 @@
-export class CategoriaDTO {
-  private idCategoria: number;
-  private nombre: string;
-  private categoriaPadre: CategoriaDTO | null;
-  private imagen: string | null;
-}
